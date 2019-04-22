7465b384c65b56a290884ea378688ad9530d18f5
张盼 <pan.zhang@yi23.net>
2019-04-22 13:29:45 +0800

    Synchronized demo

--- stage-4/stage-4-lesson-1/src/main/java/com/sxzhongf/deep/in/java/thread/posix/SynchronizedDemo.java
+++ stage-4/stage-4-lesson-1/src/main/java/com/sxzhongf/deep/in/java/thread/posix/SynchronizedDemo.java
@@ -14,17 +14,22 @@ import java.util.concurrent.locks.ReentrantLock;
 public class SynchronizedDemo {
 
     //加锁和不加锁的区别
+    // Lock == pthread_mutex_t
     static Lock lock = new ReentrantLock();
     static int counter = 0;
 
     public static void main(String[] args) throws InterruptedException {
 
+        // Condition = pthread_cond_t
         Condition condition = lock.newCondition();
         // await() 和signal() 或signalAll()
         // 使用前提 ：Lock#lock()
 
         // Object#wait() 和Notify()或NotifyAll()
         // 使用前提：synchronized(object) -> 中
+        synchronized(Object.class){
+//            Object.class.wait();
+        }
 
         Thread t1 = new Thread(SynchronizedDemo::addCounter);
         Thread t2 = new Thread(SynchronizedDemo::addCounter);
@@ -37,11 +42,11 @@ public class SynchronizedDemo {
     }
 
     private static void addCounter() {
-        lock.lock();
+        lock.lock(); //pthread_mutex_lock()
         System.out.println(getThreadPrefix() + "Before Counter : " + counter);
         counter++;
         System.out.println(getThreadPrefix() + "After Counter : " + counter);
-        lock.unlock();
+        lock.unlock();// pthread_mutex_unlock()
     }
 
     private static String getThreadPrefix() {
