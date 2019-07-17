9b326e54dca09b063e984783e2dcf7a10f820110
IsaacZhang <zhangpan0614@126.com>
2019-07-17 15:16:08 +0800

    每日一题之操作运算符的优先级～

---
+++ daily-question/src/main/java/com/sxzhongf/daily/question/july/Ask20190717_Priority.java
@@ -0,0 +1,22 @@
+package com.sxzhongf.daily.question.july;
+
+/**
+ * Ask20190717_Priority for TODO
+ *
+ * @author <a href="mailto:magicianisaac@gmail.com">Isaac.Zhang | 若初</a>
+ * @since 2019/7/17
+ */
+public class Ask20190717_Priority {
+//    static final String pig = "length: 10";
+//    static final String dog = "length: " + pig.length();
+
+
+    public static void main(String[] args) {
+        final String pig = "length: 10";
+        final String dog = "length: " + pig.length();
+        System.out.println("Animals are equal: "
+                + (pig == dog));
+//        System.out.println("Animals are equal: "
+//                + pig.equals(dog));
+    }
+}
